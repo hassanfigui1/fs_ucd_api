80cbcb6aa812e75f6e796c7342be2e04b2946096
hassan figuigui <hassan.figuigui7@aiesec.net>
2024-03-04 16:43:04 +0100

    admin rest api & fs ucd rest api

---
+++ fs_ucd_admin_rest_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FsUcdAdminRestApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fs_ucd_admin_rest_api'
---
+++ fs_ucd_rest_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FsUcdRestApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fs_ucd_rest_api'
